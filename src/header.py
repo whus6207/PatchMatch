9e7a34e10f9dc35baaffe04ee63f626871278503
Po-Hsien <ddmail2009@gmail.com>
2014-06-14 15:10:15 +0800

    add mask and getNNF

--- src/header.py
+++ src/header.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as npl
 
-
 def RGBtoGray(img):
-  return np.dot(img[..., :3], [0.299, 0.587, 0.144])
+  gray = np.dot(img[..., :3], [0.299, 0.587, 0.144])
+  gray[gray > 255] = 255
+  gray[gray <   0] = 0
+  return gray.astype("uint8")
 def GraytoRGB(img):
   if (len(img.shape) == 3 and img.shape[2] > 1):
     print 'only 1-channel grey scale image can be converted to RGB'
@@ -12,3 +14,17 @@ def GraytoRGB(img):
   colorImg[:, :, 1] = img
   colorImg[:, :, 2] = img
   return colorImg
+
+# need reshape after return
+def getNNF(filepath):
+  file = read(filepath, 'rb')
+  a = []
+  while True:
+    rgb = file.read(3)
+    if rgb == '':
+      break
+    data = rgb[0]<<16 | rgb[1]<<8 | rgb[2]
+    by = data >> 12 
+    bx = data & 0x00000FFF
+    a.append((by, bx))
+  return np.array(a)
