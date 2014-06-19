0420d5f5bb06488af64029b7183502bf694aa72d
ddmail2009 <ddmail2009@gmail.com>
2014-06-19 19:07:01 +0800

    retarget almost done

--- src/retarget.py
+++ src/retarget.py
@@ -5,18 +5,32 @@ import matplotlib.pyplot as pl
 import scipy.ndimage
 import math
 
-num_it=15
+num_it=2
 patch_size=7
 w_rate=0.95
-h_rate=0.95
-NNF_dll.setPatchW(7)
-a=pl.imread("../image/seam_carving.jpg")[::2, ::2][::2, ::2][::2, ::2]
+h_rate=1
+NNF_dll.setPatchW(patch_size)
+
+a=pl.imread("../image/dog.jpg")[::2, ::2][::2, ::2][::2, ::2]
+#print "a1 shape=", a1.shape
+#a=np.zeros((a1.shape[0]+patch_size-1, a1.shape[1]+patch_size-1, 3), dtype=a1.dtype)
+#a[patch_size/2 : -patch_size/2+1, patch_size/2 : -patch_size/2+1]=a1
 bitmap1=NNF_dll.np2Bitmap(a)
-b=cv2.resize(a,  (int(a.shape[1]*h_rate), int(a.shape[0]*w_rate) ))
+
+#b=cv2.resize(a1,  (int(a1.shape[1]*w_rate), int(a1.shape[0]*h_rate)))
+b=cv2.resize(a,  (int(a.shape[1]*w_rate), int(a.shape[0]*h_rate)))
+#pl.imshow(b1)
+#pl.show()
+#print "b1 shape=", b1.shape
+#b=np.zeros((b1.shape[0]+patch_size-1,b1.shape[1]+patch_size-1,3), dtype=b1.dtype)
+#b[patch_size/2:-patch_size/2+1, patch_size/2:-patch_size/2+1]=b1
+
 
 print "a shape=", a.shape
 print "b shape=", b.shape
 
+#pl.imshow(b)
+#pl.show()
 for it in range(num_it):
     bitmap2=NNF_dll.np2Bitmap(b)
     com_ann, com_annd=NNF_dll.patchmatch(bitmap1,bitmap2)
@@ -31,26 +45,27 @@ for it in range(num_it):
 
     Ns=(a.shape[0]-patch_size)*(a.shape[1]-patch_size)*1.0
     Nt=(b.shape[0]-patch_size)*(b.shape[1]-patch_size)*1.0
-    m=patch_size**2
+    m=1#(patch_size)**2
     # calculate value of each pixel
-    for i in range(b.shape[0]-patch_size):
-        for j in range(b.shape[1]-patch_size):
+    for i in range(patch_size/2, b.shape[0]-(patch_size/2)+1):
+        for j in range(patch_size/2, b.shape[1]-(patch_size/2)+1):
             p_com = np.zeros(3)
             p_coh = np.zeros(3)
-            n=len(com_map[i][j])
+            n=0#len(com_map[i][j])
 
-            for k in range(n):
-                p_com+=a[ int(com_map[i][j][k][0]) ][ int(com_map[i][j][k][1]) ]
-            for x in range(i, i+patch_size):
-                for y in range(j, j+patch_size):
-                    p_coh+=a[ int(coh_ann[x][y][0]) ][ int(coh_ann[x][y][1]) ]
+            #for k in range(n):
+            #    p_com+=a[ int(com_map[i][j][k][0]) ][ int(com_map[i][j][k][1]) ]
+            for x in range (1):#-patch_size/2, patch_size/2):
+                for y in range (1):#-patch_size/2, patch_size/2):
+                    p_coh+=a[ int(coh_ann[i+x][j+y][0])-x ][ int(coh_ann[i+x][j+y][1])-y ]
 
             b[i][j]=((p_com/Ns+p_coh/Nt)/(n/Ns+m/Nt)).astype("int32")
+    #pl.subplot(num_it/2+1,2,it+1).imshow(b)
 
-print b.shape
+pl.subplot(2,1,1).imshow(b)
 b=b[:-patch_size, :-patch_size]
-pl.subplot(3,1,1).imshow(a)
-pl.subplot(3,1,2).imshow(b)
-pl.subplot(3,1,3).imshow(cv2.resize(a,(int(a.shape[1]*h_rate), int(a.shape[0]*w_rate))))
+#pl.subplot(num_it/2+1,2,num_it+2).imshow(a)
+#pl.subplot(1,2,1).imshow(b)
+pl.subplot(2,1,2).imshow(a)
 pl.show()
 exit(1)
